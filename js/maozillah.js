10298701fdd0ac953f4902d6a85c42da076abee8
maozillah <kayexmao@gmail.com>
2015-11-07 15:43:14 -0500

    removed touch js code

--- js/maozillah.js
+++ js/maozillah.js
@@ -89,23 +89,23 @@
 
   // remove hovers for mobile
 
-  var touch = window.ontouchstart
-            || navigator.MaxTouchPoints > 0
-            || navigator.msMaxTouchPoints > 0;
+//   var touch = window.ontouchstart
+//             || navigator.MaxTouchPoints > 0
+//             || navigator.msMaxTouchPoints > 0;
 
-if (touch) { // remove all :hover stylesheets
-    try { // prevent crash on browsers not supporting DOM styleSheets properly
-        for (var si in document.styleSheets) {
-            var styleSheet = document.styleSheets[si];
-            if (!styleSheet.rules) continue;
+// if (touch) { // remove all :hover stylesheets
+//     try { // prevent crash on browsers not supporting DOM styleSheets properly
+//         for (var si in document.styleSheets) {
+//             var styleSheet = document.styleSheets[si];
+//             if (!styleSheet.rules) continue;
 
-            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-                if (!styleSheet.rules[ri].selectorText) continue;
+//             for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+//                 if (!styleSheet.rules[ri].selectorText) continue;
 
-                if (styleSheet.rules[ri].selectorText.match(':hover')) {
-                    stylesheet.deleteRule(ri);
-                }
-            }
-        }
-    } catch (ex) {}
-}
+//                 if (styleSheet.rules[ri].selectorText.match(':hover')) {
+//                     stylesheet.deleteRule(ri);
+//                 }
+//             }
+//         }
+//     } catch (ex) {}
+// }
